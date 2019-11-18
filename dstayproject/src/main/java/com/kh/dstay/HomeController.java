245b52ed66598c62dc9e40686273f6604a03fbe4
user2 <user2@KH_I>
2019-11-18 19:59:45 +0900

    asd

--- dstayproject/src/main/java/com/kh/dstay/HomeController.java
+++ dstayproject/src/main/java/com/kh/dstay/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		int a = 0;
-		String b="";
+		String b="s";
 		
 		return "4_jong/mainPage";
 	}
